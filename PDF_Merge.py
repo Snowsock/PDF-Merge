206bac64ce53c9f187567fe7c4363c2777758d2a
Snowsock <iha001@uib.no>
2022-02-04 18:23:39 +0100

    We have a GUI, of sorts

--- PDF_Merge.py
+++ PDF_Merge.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfFileMerger
-import tkinter
+import tkinter as tk
 
 
 def pdf_merger():
@@ -12,8 +12,11 @@ def pdf_merger():
 
 
 def start_gui():
-    main_window = tkinter.Tk()
+    main_window = tk.Tk()
     main_window.title("PDF Merger")
+    button = tk.Button(main_window, text='Stop', width=25, command=main_window.destroy)
+    button.pack()
+    main_window.mainloop()
 
 
 if __name__ == "__main__":
