4f649a92b5b1d1c549e56e7f6e4e531e868b18ac
Snowsock <iha001@uib.no>
2022-02-04 19:05:36 +0100

    can list directories

--- PDF_Merge.py
+++ PDF_Merge.py
@@ -1,11 +1,17 @@
-from PyPDF2 import PdfFileMerger
+import os
+import sys
+from PyPDF2 import PdfFileMerger as PfM
+
 
 def pdf_merger():
     pdfs = ['pdf1.pdf', 'pdf2.pdf']
-    merger = PdfFileMerger()
+    merger = PfM()
     for pdf in pdfs:
         merger.append(pdf)
     merger.write("result.pdf")
     merger.close
 
 
+if __name__ == '__main__':
+    listed_directories = os.walk('.', False)
+    print([x[0] for x in listed_directories])
